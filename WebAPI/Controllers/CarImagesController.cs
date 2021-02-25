95dbc62eb105267ce0b19325b3e31f61d9715fc3
dilara153232@gmail.com <dilara153232@gmail.com>
2021-02-26 01:29:51 +0300

    25/02/2021

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -13,10 +17,43 @@ namespace WebAPI.Controllers
     public class CarImagesController : ControllerBase
     {
         ICarImageService _carImageService; //field
+        IWebHostEnvironment _webHostEnvironment;
 
-        public CarImagesController(ICarImageService carImageService)
+        public CarImagesController(ICarImageService carImageService, IWebHostEnvironment webHostEnvironment)
         {
             _carImageService = carImageService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost("Add")]
+        public string Post([FromForm] FileUpload objectFile)
+        {
+            try
+            {
+                if (objectFile.files.Length > 0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(path + objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "Yükleme başarılı";
+                    }
+                }
+                else
+                {
+                    return "Yükleme başarısız!";
+                }
+            }
+            catch (Exception exception)
+            {
+
+                return exception.Message; ;
+            }
         }
 
         [HttpGet("getall")]
